712bda44c95b1f2a6e0bb5640beef09080f797c3
Tulio Contiliani <tuliobcontiliani@gmail.com>
2021-02-14 02:18:45 -0300

    Adicionando funções dos botões

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -18,9 +18,28 @@ map.on('click', (event) => {
     const lat = event.latlng.lat;
     const lng = event.latlng.lng;
 
+    document.querySelector('[name=lat]').value = lat;
+    document.querySelector('[name=lng]').value = lng;
+
     // remove icon
     marker && map.removeLayer(marker);
 
     // add icon layer
     marker = L.marker([lat, lng], { icon }).addTo(map);
 });
+
+// add photos field
+function addPhotoField() {
+    // get photos container #images
+    const container = document.querySelector('#images');
+    // get container and duplicate .new-image
+    const fieldsContainer = document.querySelectorAll('.new-upload');
+    // clone last added image
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true);
+
+    // clear the field before adding it to the image container
+    console.log(newFieldContainer.children);
+
+    // add clone to container #images
+    container.appendChild(newFieldContainer);
+}
